db913a7bb34bc7b242217f73d269f8f05f6c10f3
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-04 14:34:43 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,6 @@ let rpsArray = ["rock", "paper", "scissors"];
 let input = prompt("Type rock, paper or scissors to start game.");
 console.log(`You have selected ${input}`);
 
-//the above input action starts the game and the computer selects from array
-//learn how to use input above to trigger below action - computer selects random
 //declares random element variable to select from array and print to console
 const randElem = Math.floor(Math.random() * 3);
 console.log("the computer has selected", randElem, rpsArray[randElem]);
