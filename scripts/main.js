184827358d7a779f835d98425ef5e2ee5d118ccd
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-04 15:17:30 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,21 @@ if (randElem == "0" && input == "rock") {
 console.log("It's a draw, refresh to try again."); 
 }
 
-//print either "player wins" or "computer wins"
-console.log(``);
+if (randElem == 1 && input == "paper") {
+  console.log("It's a draw, refresh to try again."); 
+}
+
+if (randElem == 2 && input == "scissors") {
+  console.log("It's a draw, refresh to try again");
+}
+
+if (randElem == 0 && input == "paper") {
+  console.log("Paper beats rock, Paper Wins!");
+}
+
+if (randElem == 0 && input == "scissors") {
+  console.log("Rock beats scissors, Rock Wins!");
+}
+
 //asks if user wants to play again
 console.log(`Try Again? Refresh the page if you're feeling lucky`);
