00a8f633bf761df6373c126a385cc33d91cb0f75
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-04 13:57:04 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
-function conputerPlay () {
-    
+function itemSelect() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+
+function computerPlay () {
+
 }
+
+//code ideas from paper algorithm
+//to return random number from array
+return Math.floor(Math.random() * number);
+
+//print either "player wins" or "computer wins"
+console.log(``);
+//asks if user wants to play again
+console.log(`Try Again?`);
