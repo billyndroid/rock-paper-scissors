f585636338e93a6f90d77acf1a1a0af38bde1ee3
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-04 14:33:51 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,21 @@
 
 //code ideas from paper algorithm
-// declare array with three elements
+// declares array with three elements
 let rpsArray = ["rock", "paper", "scissors"];
 
-//declare random element variable to select from array and print to console
+//user inputs selection from array and selection written to console
+let input = prompt("Type rock, paper or scissors to start game.");
+console.log(`You have selected ${input}`);
+
+//the above input action starts the game and the computer selects from array
+//learn how to use input above to trigger below action - computer selects random
+//declares random element variable to select from array and print to console
 const randElem = Math.floor(Math.random() * 3);
-console.log(randElem, rpsArray[randElem]);
+console.log("the computer has selected", randElem, rpsArray[randElem]);
 
-//user inputs selection from array
-
+//compare user selection (input) with computer random selection (randElem) (using if/else)
 
 //print either "player wins" or "computer wins"
 console.log(``);
 //asks if user wants to play again
-console.log(`Try Again?`);
+console.log(`Try Again? Refresh the page if you're feeling lucky`);
